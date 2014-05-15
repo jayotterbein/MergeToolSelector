207b4eed99f1dd96544ef976b3d90b19787efe3b
Jay Otterbein <jay.otterbein@headspring.com>
2014-05-15 13:06:59 -0500

    removed unused usings

--- MergeToolSelector/Utility/CommandLineParser.cs
+++ MergeToolSelector/Utility/CommandLineParser.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Windows.Forms;
 using MergeToolSelector.Forms;
 using MergeToolSelector.Utility.FileExtensions;
 
