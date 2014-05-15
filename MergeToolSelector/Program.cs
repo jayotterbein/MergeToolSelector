e4a3e037003f06f7d1c9d004e15947ac6f7b1033
Jay Otterbein <jay.otterbein@headspring.com>
2014-05-15 09:45:29 -0500

    added logging to program main()

--- MergeToolSelector/Program.cs
+++ MergeToolSelector/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using MergeToolSelector.Utility;
 using MergeToolSelector.Utility.FileExtensions;
 using MergeToolSelector.Utility.Settings;
@@ -13,11 +6,16 @@ using NLog;
 
 namespace MergeToolSelector
 {
-    public class Program
+    static public class Program
     {
         [STAThread]
         public static void Main(string[] args)
         {
+            var logger = LogManager.GetCurrentClassLogger();
+            logger.Trace("Main():");
+            for (var i = 0; i < args.Length; i++)
+                logger.Trace(string.Format("  {0}: {1}", i, args[i]));
+
             var processExecuter = new ProcessExecuter();
             var messageDisplayer = new FormDisplayer();
             var fileProvider = new FileProvider();
